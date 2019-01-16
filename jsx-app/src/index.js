a44665281b227017e43dbee9387bf69aec48252f
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 11:07:43 -0700

    step 1: import react and reactdom

--- jsx-app/src/index.js
+++ jsx-app/src/index.js
@@ -1,5 +1,8 @@
 //want to figure out how to use React to get content on the screen
 //Import the React and ReactDOM libraries
+//import gives access to other libraries or other written code
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 //Create a React Component
 
