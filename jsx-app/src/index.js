4ec4d5bd8d7e526865ebb36a86c2b4f6a63d3dba
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 11:05:22 -0700

    create index.js file

---
+++ jsx-app/src/index.js
@@ -0,0 +1,6 @@
+//want to figure out how to use React to get content on the screen
+//Import the React and ReactDOM libraries
+
+//Create a React Component
+
+//Take React Component and shoe it on the screen
