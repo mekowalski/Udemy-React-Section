9f2dd93bd718cf5c3978fc364771244f68dbf650
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 11:14:26 -0700

    minor notes on import and require

--- jsx-app/src/index.js
+++ jsx-app/src/index.js
@@ -1,9 +1,10 @@
 //want to figure out how to use React to get content on the screen
 //Import the React and ReactDOM libraries
-//import gives access to other libraries or other written code
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 //Create a React Component
+
 
 //Take React Component and shoe it on the screen
